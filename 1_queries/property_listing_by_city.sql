b0c2be52ae7dcd4cfedef12c3e2c58b3e2c072b4
Neihy Rincon <neihy.rincon@gmail.com>
2020-04-30 20:09:49 +0000

    adding properties

--- 1_queries/property_listing_by_city.sql
+++ 1_queries/property_listing_by_city.sql
@@ -6,11 +6,3 @@ GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
 ORDER BY cost_per_night
 LIMIT 10;
-
-
-SELECT properties.*, avg(property_reviews.rating) as average_rating
-FROM properties
-JOIN property_reviews ON properties.id = property_id WHERE city LIKE '%ancouv%' AND cost_per_night >= 1000 AND cost_per_night <= 2000 
-GROUP BY properties.id
-ORDER BY cost_per_night
-LIMIT 20;
